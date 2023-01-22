1cc4c9728c521967b36aab81f9f36d1645bf82b8
AbdulRahman Khalid <122513436+Aabdulr7man@users.noreply.github.com>
2023-01-22 21:11:06 +0300

    feat: two functions that creates tables.
    Data table and faq table added to the program. By calling functions it will create a table if not exists.

---
+++ main.py
@@ -0,0 +1,15 @@
+import sqlite3
+
+
+def create_faq_table():
+    conn = sqlite3.connect('mydb.db')
+    cur = conn.cursor()
+    cur.execute("CREATE TABLE IF NOT EXISTS 'faq' (text TEXT)")
+
+
+def create_data_table():
+    conn = sqlite3.connect('mydb.db')
+    cur = conn.cursor()
+    cur.execute(
+        "CREATE TABLE IF NOT EXISTS 'data' (name VARCHAR (30) PRIMARY KEY NOT NULL,address VARCHAR (255) NOT NULL,bio VARCHAR (255) ")
+    conn.commit()
